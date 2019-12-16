576b99ed259c27aa63da27e1a68ae4822d0f3c6d
Rockford Lhotka <rocky@lhotka.net>
2019-12-16 16:20:43 -0600

    Initial commit

---
+++ BlazorClientExample/Client/PersonEdit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BlazorClientExample.Client
+{
+  public class PersonEdit
+  {
+    public string Name { get; set; }
+    public DateTime Birthday { get; set; }
+    public int Age { get => (DateTime.Now - Birthday).Days / 365; }
+  }
+}
