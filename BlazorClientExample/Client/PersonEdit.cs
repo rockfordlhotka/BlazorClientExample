b943f88dec5277996000ba4f3e4bf6d20bee5e85
Rockford Lhotka <rocky@lhotka.net>
2020-02-20 17:33:04 -0800

    Add SimpleForm page

--- BlazorClientExample/Client/PersonEdit.cs
+++ BlazorClientExample/Client/PersonEdit.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorClientExample.Client
 {
   public class PersonEdit
   {
+    [Required]
     public string Name { get; set; }
     public DateTime Birthday { get; set; }
     public int Age { get => (DateTime.Now - Birthday).Days / 365; }
